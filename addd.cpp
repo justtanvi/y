facf8fa6b85dfd57b729fe0abda2fd594bb7eee1
Tanvi <tsawant711@gmail.com>
2024-04-07 20:17:02 +0530

    Committed

--- addd.cpp
+++ addd.cpp
@@ -6,9 +6,15 @@ void addit(int x ,int y)
 	cout<<"Addition :"<<x+y<<endl;
 }
 
+void mult(int x , int y)
+{
+	cout<<"Multiplication :"<<x*y<<endl;
+}
+
 int main()
 {
 	addit(5,7);
+	mult(6,7);
 	return 0;
 	
 }
